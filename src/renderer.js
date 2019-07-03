a35006eecb6c014e6913ba94e5c98002ac1c5f09
fzaman6505 <fzaman6505@gmail.com>
2019-07-02 19:56:52 -0700

    updated Temperature Converter

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,12 @@
-function celciusToFahrenheit(){
-    let celcius = document.getElementById('celcius').value;
-    let fahrenheit = (celcius* 9/5) + 32;
+function celsiusToFahrenheit(){
+    let celsius = document.getElementById('celsius').value;
+    let fahrenheit = (celsius* 9/5) + 32;
     document.getElementById('fahrenheit').value = fahrenheit;
 
 }
 
-function fahrenheitToCelcius(){
+function fahrenheitToCelsius(){
     let fahrenheit = document.getElementById('fahrenheit').value;
-    let celcius = (fahrenheit - 32) * 5/9
-    document.getElementById('celcius').value = celcius;
+    let celsius = (fahrenheit - 32) * 5/9
+    document.getElementById('celsius').value = celsius;
 }
